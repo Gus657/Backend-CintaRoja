bb09be44b477201de8e06a585e461841a447d692
Gus657 <ggusperezg@gmail.com>
2020-04-14 20:29:55 -0600

    Conexion Mongo

--- database/index.js
+++ database/index.js
@@ -1,3 +1,12 @@
 /*
 MongoDB Atlas URL
 */
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://657:657@gusdev-m496e.mongodb.net/api?retryWrites=true&w=majority', 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+.then(()=>console.log('DB Connected'))
+.catch(()=>console.log('Error on Connection'));
