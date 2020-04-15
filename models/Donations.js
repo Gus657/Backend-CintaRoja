88d6070d3ab6282de0e0894a5acfae379bf95c5e
siIvin <garavito.silvia@gmail.com>
2020-04-15 13:58:45 -0600

    donation model

--- models/Donations.js
+++ models/Donations.js
@@ -1,3 +1,14 @@
+const mongoose= require('mongoose');
+const Schema = mongoose.Schema;
+const donationSchema= new Schema({
+    suscriber: {type: mongoose.Schema.ObjectId, ref: Sucribers, required: true},
+    amount: Number,
+    message: String,
+})
+
+const Donations = mongoose.model(donationSchema);
+module.exports = Donations
+
 /*
 Hi T.T
 
