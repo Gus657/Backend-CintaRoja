6d9846553264cac58fb4e1cdb99edbf9921d3424
Gus657 <ggusperezg@gmail.com>
2020-04-16 20:06:13 -0600

    Cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000 
 require('./database');
 
 //middelwares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 //Routes
 app.use(require('./routes/Suscribers'));
 app.use(require('./routes/Donations')); 
